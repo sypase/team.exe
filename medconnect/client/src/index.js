d60a38fab74d95e12f05117e87f87f7b8296ddf7
Arjan789 <144706090+Arjan789@users.noreply.github.com>
2023-09-17 10:12:12 +0545

    Update index.js
    index.js coment

--- medconnect/client/src/index.js
+++ medconnect/client/src/index.js
@@ -1,3 +1,4 @@
+/* importing necessary tools */
 import React from "react";
 import 'antd/dist/antd.min.css'
 import ReactDOM from "react-dom/client";
@@ -6,13 +7,14 @@ import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
-
+// Getting the DOM element with the id "root"
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// Rendering the main App component wrapped in the Redux Provider component
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
 );
 
-
+// Reporting web vitals 
 reportWebVitals();
